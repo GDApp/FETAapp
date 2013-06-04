ea56e9cb0de468ae68f6a762b8193ec95a9ad60c
GDApp <tomek.twa@gmaio.com>
2013-06-04 04:28:46 -0700

    Update mapa.js

--- www/js/mapa.js
+++ www/js/mapa.js
@@ -1,11 +1,11 @@
 $("#onMap").click(function(){
 
 if (animDir == 1) {
-	&("#content1").empty();
+	$("#content1").empty();
 	$('<iframe id="canvmapa" src="mapa.html">').appendTo('#content1');
 }
 else {
-	&("#content2").empty();
+	$("#content2").empty();
 	$('<iframe id="canvmapa" src="mapa.html">').appendTo('#content2');
 }
 
