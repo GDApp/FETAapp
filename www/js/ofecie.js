6998d174e9a211e7dbae41d19125c2e8e97d8409
Anatol <ank993@gmail.com>
2013-06-11 03:17:12 +0200

    xxx

--- www/js/ofecie.js
+++
@@ -1,7 +0,0 @@
-$(".imgTeatry").click(function(){
-	$('body').append("<div class='pic'><img src=" + $(this).attr("src") + " class=" + "'insidepic'" + "></div>");
-});
-
-$("body").delegate(".pic", "click", function() {
-	$(".pic").remove();
-});
